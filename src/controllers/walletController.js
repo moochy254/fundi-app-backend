c1c552a617646d7de2e3664abf176e46d974503f
moochy254 <bmuchendu254@gmail.com>
2026-05-25 15:20:11 -0700

    Fix duplicate imports in walletController

--- src/controllers/walletController.js
+++ src/controllers/walletController.js
@@ -4,49 +4,6 @@ const {
   requestWithdrawal,
   createWallet
 } = require('../models/walletModel');
-
-// @desc Get provider wallet
-const getWallet = async (req, res) => {
-  try {
-    if (req.user.role !== 'provider') {
-      return res.status(403).json({ message: 'Only providers can access wallet' });
-    }
-
-    let wallet = await getWalletByProviderId(req.user.id);
-
-    if (!wallet) {
-      wallet = await createWallet(req.user.id);
-    }
-
-    res.json(wallet);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-};
-
-// @desc Get wallet transactions
-const getTransactions = async (req, res) => {
-  try {
-    if (req.user.role !== 'provider') {
-      return res.status(403).json({ message: 'Only providers can access transactions' });
-    }
-
-    const transactions = await getWalletTransactions(req.user.id);
-    res.json(transactions);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-};
-
-// @desc Request withdrawal
-const {
-  getWalletByProviderId,
-  getWalletTransactions,
-  requestWithdrawal,
-  createWallet
-} = require('../models/walletModel');
 const { b2cPayment } = require('../config/mpesa');
 const pool = require('../config/db');
 
